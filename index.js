1bfb8ff39dc7fb82839d0a319e3190b01eddee83
Maksim Kryshneu <maks.kryshnev@gmail.com>
2019-11-04 16:47:36 +0300

    feat: add some functional to control buttons

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-let ruKeyBoardDown = [
-  ["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
-  ["Tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "&#92;"],
-  ["CapsLock", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "Enter"],
-  ["Shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".", "&#8593;", "Shift"],
-  ["Ctrl", "Win", "Alt", " ", "Alt", "&#8592;", "&#8595;", "&#8594;", "Ctrl"],
+const ruKeyBoardDown = [
+  ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+  ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '&#92;'],
+  ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
+  ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#8593;', 'Shift'],
+  ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '&#8592;', '&#8595;', '&#8594;', 'Ctrl'],
 ];
-let ruKeyBoardUp = [
-  ["Ё", "!", '"', "№", ";", "%", ":", "?", "", "(", ")", "", "+", "Backspace"],
+const ruKeyBoardUp = [
+  ['Ё', '!', '"', "№", ";", "%", ":", "?", "", "(", ")", "", "+", "Backspace"],
   ["Tab", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "/"],
   ["CapsLock", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "Enter"],
   ["Shift", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ".", "&#8593;", "Shift"],
   ["Ctrl", "Win", "Alt", " ", "Alt", "&#8592;", "&#8595;", "&#8594;", "Ctrl"],
 ];
-let engKeyBoardDown = [
+const engKeyBoardDown = [
   ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
   ["Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "&#92;"],
   ["CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter"],
   ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "&#8593;", "Shift"],
   ["Ctrl", "Win", "Alt", " ", "Alt", "&#8592;", "&#8595;", "&#8594;", "Ctrl"],
 ];
-let engKeyBoardUp = [
+const engKeyBoardUp = [
   ["~", "!", "@", "#", "$", "%", "^", "&", "", "(", ")", "", "+", "Backspace"],
   ["Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|"]
     ["CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter"],
   ["Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "&#8593;", "Shift"],
   ["Ctrl", "Win", "Alt", " ", "Alt", "&#8592;", "&#8595;", "&#8594;", "Ctrl"],
 ];
-let keysDescription = [
-  ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Digit-", "Equal=", "Backspace"],
+const keysDescription = [
+  ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
   ["Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash"],
   ["CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter"],
   ["ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ArrowUp", "ShiftRight"],
-  ["ControlLeft", "MetaLeft", "AltLeft", "Space", "AltRight", "ArrowLeft", "ArrowDown", "ArrowRight", "ControlRight"],
+  ['ControlLeft', "MetaLeft", "AltLeft", "Space", "AltRight", "ArrowLeft", "ArrowDown", "ArrowRight", "ControlRight"],
 ]
 
 function createWrapper() {
@@ -58,31 +58,72 @@ function createElements() {
     }
   }
 }
-createElements();
-document.addEventListener("keydown", writeText);
-document.addEventListener("keyup", afterWriteText);
-document.addEventListener("click", click);
-function writeText(e){
-  e.preventDefault();
-  const input = document.querySelector('.area');
-  const key = document.querySelector(`#${e.code}`);
-  input.value += key.innerText;
-  key.style.backgroundColor = "red";
-  key.style.transform = "translateY(4px)";
-}
-function afterWriteText (e){
-  e.preventDefault();
-  const key = document.querySelector(`#${e.code}`);
-  key.style.backgroundColor = "black";
-  key.style.transform = "translateY(0px)";}
 
-function click(e){
+
+createElements();
+document.addEventListener('keydown', writeText);
+document.addEventListener('keyup', afterWriteText);
+document.addEventListener('click', click);
+
+function writeText(e) {
   e.preventDefault();
   const input = document.querySelector('.area');
-  let target = e.target;
-  if (target.className === 'key'){
+  const key = document.querySelector(`#${e.code}`);
+  input.focus();
+  key.style.backgroundColor = 'red';
+  key.style.transform = 'translateY(4px)';
+  function setCaretPosition(ctrl, pos) {
+    if (ctrl.setSelectionRange) {
+      ctrl.focus();
+      ctrl.setSelectionRange(pos, pos);
+    }
+  }
+  switch (key.id) {
+    case ('Tab'):
+      input.value += '\t';
+      break;
+    case ('Enter'):
+      input.value += '\n';
+      break;
+    case ('Space'):
+      input.value += ' ';
+      break;
+    case ('Backspace'):
+      input.value = input.value.slice(0, input.value.length - 1);
+      break;
+    case ('ArrowLeft'):
+      setCaretPosition(input, input.value.length-1);
+      break;
+    case ('ArrowRight'):
+      setCaretPosition(input, input.value.length+1);
+      break;
+    case ('ArrowDown'):
+      setCaretPosition(input, 0);
+      break;
+    case ('ArrowUp'):
+      setCaretPosition(input, input.value.length);
+      break;
+    case ('CapsLock'):
+    default:
+      input.value += key.innerText;
+  }
+}
+
+function afterWriteText(e) {
+  e.preventDefault();
+  const key = document.querySelector(`#${e.code}`);
+  key.style.backgroundColor = 'black';
+  key.style.transform = 'translateY(0px)';
+}
+
+function click(e) {
+  e.preventDefault();
+  const input = document.querySelector('.area');
+  const target = e.target;
+  if (target.className === 'key') {
     input.value += `${e.target.innerText}`;
-    key.style.backgroundColor = "red";
-    key.style.transform = "translateY(4px)";}
+    key.style.backgroundColor = 'red';
+    key.style.transform = 'translateY(4px)';
+  }
 
 }
